3a8ff0ad3463835fe889c72d72e804f787403127
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-07-24 17:10:18 +0200

    Adjust mobile menu bars

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const hamburger = document.querySelector('.burger-menu');
 const navMenu = document.querySelector('.mobile-nav');
 
-document.querySelector('.nav-link').forEach(n =>
-  n.addEventListener('click', () => {
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelector('.nav-link').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
   })
