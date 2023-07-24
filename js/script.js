c2bb6b9aeefc3319871239d17cc5900357dea50a
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-07-24 16:57:55 +0200

    Add DOM events to mobile menu

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 const hamburger = document.querySelector('.burger-menu');
 const navMenu = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
