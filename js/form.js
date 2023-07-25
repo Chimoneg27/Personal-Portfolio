713e1e0f370ce1a6ec346fbf4a63bb1302ab1a3d
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-07-25 17:13:56 +0200

    Fix linter errors

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@ const form = document.getElementById('portfolio-form');
 const emailInput = document.getElementById('email-input');
 const errorMsg = document.getElementById('error-text');
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
   if (emailInput.value === emailInput.value.toLowerCase()) {
     return true;
   }
