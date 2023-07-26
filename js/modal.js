149e2fd4784b72ba349a6c059816773624a29b5e
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-07-26 16:26:25 +0200

    Add array of JavaScript objects

---
+++ js/modal.js
@@ -0,0 +1,68 @@
+const cardsData = [
+    {
+      name: "Multi Post Stories",
+      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+      descriptionOneModal: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sstandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+      descriptionTwoModal: "    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      featuredImage: 'images/pop up mobile.png',
+      technologies: ['HTML', 'Bootstrap', 'Ruby'],
+      liveVersion: 'https://chimoneg27.github.io/Personal-Portfolio/',
+      sourceLink: 'https://github.com/example/card1',
+      imageDesktop: "images/desktop-images/pop up desktop.png"
+    },
+    {
+      name: "Multi Post Stories",
+      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+      descriptionOneModal: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sstandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.", 
+      descriptionTwoModal: "    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      featuredImage: 'images/pop up mobile.png',
+      technologies: ['HTML', 'Bootstrap', 'Ruby'],
+      liveVersion: 'https://chimoneg27.github.io/Personal-Portfolio/',
+      sourceLink: 'https://github.com/example/card2',
+      imageDesktop: "images/desktop-images/pop up desktop.png"
+    },
+    {
+      name: "Multi Post Stories",
+      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+      descriptionOneModal: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sstandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.", 
+      descriptionTwoModal: "    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      featuredImage: 'images/pop up mobile.png',
+      technologies: ['HTML', 'Bootstrap', 'Ruby'],
+      liveVersion: 'https://chimoneg27.github.io/Personal-Portfolio/',
+      sourceLink: 'https://github.com/example/card2',
+      imageDesktop: "images/desktop-images/pop up desktop.png"
+    },
+    {
+      name: "Multi Post Stories",
+      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+      descriptionOneModal: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sstandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.", 
+      descriptionTwoModal: "    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      featuredImage: 'images/pop up mobile.png',
+      technologies: ['HTML', 'Bootstrap', 'Ruby'],
+      liveVersion: 'https://chimoneg27.github.io/Personal-Portfolio/',
+      sourceLink: 'https://github.com/example/card2',
+      imageDesktop: "images/desktop-images/pop up desktop.png"
+    },
+    {
+      name: "Multi Post Stories",
+      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+      descriptionOneModal: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sstandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.", 
+      descriptionTwoModal: "    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      featuredImage: 'images/pop up mobile.png',
+      technologies: ['HTML', 'Bootstrap', 'Ruby'],
+      liveVersion: 'https://chimoneg27.github.io/Personal-Portfolio/',
+      sourceLink: 'https://github.com/example/card2',
+      imageDesktop: "images/desktop-images/pop up desktop.png"
+    },
+    {
+      name: "Multi Post Stories",
+      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+      descriptionOneModal: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sstandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.", 
+      descriptionTwoModal: "    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      featuredImage: 'images/pop up mobile.png',
+      technologies: ['HTML', 'Bootstrap', 'Ruby'],
+      liveVersion: 'https://chimoneg27.github.io/Personal-Portfolio/',
+      sourceLink: 'https://github.com/example/card2',
+      imageDesktop: "images/desktop-images/pop up desktop.png"
+    }
+  ];
