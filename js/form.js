55b119f282a67a4574abc69bc23a16dfa88e96c0
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-07-26 12:00:14 +0200

    Add form input varibles

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('portfolio-form');
 const emailInput = document.getElementById('email-input');
 const errorMsg = document.getElementById('error-text');
+const fullName = document.querySelector('#fullName');
+const email = document.querySelector('#email-input');
+const textarea = document.getElementById('textArea');
 
 form.addEventListener('submit', (event) => {
   if (emailInput.value === emailInput.value.toLowerCase()) {
