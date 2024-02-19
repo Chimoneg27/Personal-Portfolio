40e171a5aa104bd4e768cccd0587d93f4bebf486
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-02-19 12:08:28 +0200

    Add more projects

--- js/modal.js
+++ js/modal.js
@@ -38,6 +38,48 @@ const cardsData = [
     sourceLink: 'https://github.com/Chimoneg27/Bookstore-React',
     imageDesktop: 'images/desktop-images/bookstore.png',
   },
+  {
+    name: 'Space Travelers Hub',
+    description:
+      'A single page app built with react-redux toolkit. The app features a home page with a list of rockets and a list of missions.',
+    descriptionOneModal:
+      'This is a single page app built with react-redux toolkit and CSS. The app features a home page with a list of rockets and a list of missions. The user can reserve a rocket and cancel the reservation. The user can also join a mission and cancel the reservation. It is fully responsive and works across multiple devices.',
+    descriptionTwoModal:
+      '',
+    featuredImage: 'images/spaceTraveler.png',
+    technologies: ['React', 'Redux', 'CSS'],
+    liveVersion: 'https://space-travelers-hub-bvt6.onrender.com/',
+    sourceLink: 'https://github.com/Chimoneg27/Travelers-Space-Hub',
+    imageDesktop: 'images/desktop-images/spaceTraveler.png',
+  },
+  {
+    name: 'Page to Page Library',
+    description:
+      'A library app built with React.js for the Front-end and Ruby on Rails backend and API. The app allows users to reserve books and also view them.',
+    descriptionOneModal:
+      'This is a library app built with React.js for the Front-end and Ruby on Rails backend and API. The app allows users to reserve books and also view them. Only the admin can add and delete books. It is fully responsive and works across multiple devices.',
+    descriptionTwoModal:
+      '',
+    featuredImage: 'images/paged.png',
+    technologies: ['React', 'ROR', 'Bootstrap'],
+    liveVersion: 'https://page-to-page.onrender.com/',
+    sourceLink: 'https://github.com/leloufadel/Page-To-page-Library-Frontend',
+    imageDesktop: 'images/desktop-images/paged.png',
+  },
+  {
+    name: 'Buggy Budgets',
+    description:
+      'A budget app built with Ruby on Rails. Allows users to track their expenses.',
+    descriptionOneModal:
+      'This is a budget app built with Ruby on Rails. Allows users to track their expenses. It is fully responsive and works across multiple devices.',
+    descriptionTwoModal:
+      '',
+    featuredImage: 'images/buggy.png',
+    technologies: ['RoR', 'Bootstrap', 'Ruby'],
+    liveVersion: 'https://buggy-budget-app.onrender.com/',
+    sourceLink: 'https://github.com/Chimoneg27/Rails-Budget-App?tab=readme-ov-file',
+    imageDesktop: 'images/desktop-images/buggy.png',
+  },
 ];
 
 function generateCardsAndPopups() {
